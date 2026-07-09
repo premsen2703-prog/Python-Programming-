902ddba983a6a8e279a954b3b1ecc248a14623ec
prem sen <prem.sen.2703@gmail.com>
2026-07-09 23:42:29 +0530

    Add python logic practice

---
+++ pattern_practice/hollow_inverted_pyamid.py
@@ -0,0 +1,10 @@
+n = int(input("Enter n :"))
+for i in  range(n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(1,2*i):
+        if i == n or i==1 or j == 1 or j == (2*i-1):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+    
